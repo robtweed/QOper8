12a92dc4c9068e7ef9416e00fc7b9ee9fd8c1c7f
robtweed <rtweed@mgateway.com>
2022-06-24 12:56:10 +0100

    Updated live example

--- examples/live/demoMessage.js
+++ examples/live/demoMessage.js
@@ -2,13 +2,6 @@ export function handler(obj, finished) {
 
   let worker = this;
 
-  postMessage({
-    type: 'custom',
-    data: {
-      foo: 'bar'
-    }
-  });
-
   finished({
     info: 'Message successfully processed by WebWorker',
     originalData: obj.data,
